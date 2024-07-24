661abe6cf3c08bee7352e7dd392cbf627ea9c605
Papaleta42 <130506964+Papaleta42@users.noreply.github.com>
2024-07-24 05:08:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Formulario from './Formulario'; // creo que el error es que se borra algo solo de los archivos
+import Formulario from './Formulario'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
